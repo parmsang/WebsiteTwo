f4bba4d7a1ea1cee120b2349fa1aaf3e044bd09b
squarebe <squarebe03@gmail.com>
2015-09-23 14:07:31 +0100

    can edit projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,17 @@
 class ProjectsController < ApplicationController
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    @project.update(update_params)
+    redirect_to showcase_path
+  end
+
+  def update_params
+    params.require(:project).permit(:title, :description, :link, :image)
+  end
+
 end
--- spec/features/project_features_spec.rb
+++ spec/features/project_features_spec.rb
@@ -28,4 +28,40 @@ feature 'showcase' do
       expect(page).to have_xpath "//img[contains(@src,'test_photo.png')]"
     end
   end
+
+  context 'editing projects' do
+    before do
+      @project = Project.new(title: 'oxfam')
+      @project.link = 'www.zombo.com'
+      @project.description = 'blah-blah'
+      @project.image = Rack::Test::UploadedFile.new('spec/fixtures/test_photo.png', 'image/png')
+      @project.save
+    end
+    scenario 'have the option for edit' do
+      visit '/'
+      expect(page).to have_link("Edit oxfam")
+    end
+
+    scenario 'there is a form with the previous information' do
+      visit '/'
+      click_link 'Edit oxfam'
+      expect(page).to have_field("Title", with: @project.title )
+      expect(page).to have_field("Description", with: @project.description )
+      expect(page).to have_field("Link", with: @project.link )
+    end
+
+    scenario 'can edit showcase' do
+      visit '/'
+      click_link 'Edit oxfam'
+      fill_in 'Title', with: 'nspca'
+      fill_in 'Description', with: 'help me!'
+      fill_in 'Link', with: 'www.wwf.com'
+      attach_file 'project_image', Rails.root.join('spec/fixtures/other_photo.png')
+      click_button 'Edit project'
+      expect(page).to have_content('nspca')
+      expect(page).to have_link("view more", href: 'www.wwf.com')
+      expect(page).to have_content('help me!')
+      expect(page).to have_xpath "//img[contains(@src,'other_photo.png')]"
+    end
+  end
 end
